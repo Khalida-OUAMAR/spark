a15abc7e9cbeff768fa45eb386396ebbcfd0bb58
Khalida OUAMAR <khalida.ouamar@outlook.com>
2021-02-09 00:16:46 +0100

    create arros for df display

--- src/main/scala/hapiness/ProcessData.scala
+++ src/main/scala/hapiness/ProcessData.scala
@@ -6,41 +6,34 @@ import org.apache.spark.sql.functions.{asc, avg, col, desc, expr, max, min}
 class ProcessData(df: DataFrame, s: SparkSession){
   val dataframe: DataFrame = df
   val spark: SparkSession = s
-  val index = Array(0,1,2)
 
   def statistics(parameter: String, df: DataFrame): DataFrame ={
 
-    println(">>>>>> df show ")
-    df.show()
     val df_Aggregates = df.agg(
-      min(col(parameter)).as("min"),
-      avg(col(parameter)).as("avg"),
-      max(col(parameter)).as("max"))
-    println(">>>>>>>>>>> show df aggregates : ")
-    df_Aggregates.show()
+      min(df(df.columns(0))).as("min"),
+      avg(df(df.columns(0))).as("avg"),
+      max(df(df.columns(0))).as("max"))
     df_Aggregates
   }
 
-  def getStatistic(parameter: String, stat: String): Seq[Double] = {
+  def getStatistic(parameter: String, stat: String): List[Double] = {
 
     val df_2015 = dataframe.select(parameter + "_2015")
     val df_2016 = dataframe.select(parameter + "_2016")
     val df_2017 = dataframe.select(parameter + "_2017")
 
-    df_2015.show()
     val stats_2015 = statistics(parameter, df_2015)
     val stats_2016 = statistics(parameter, df_2016)
     val stats_2017 = statistics(parameter, df_2017)
 
-    val stat_2015 = stats_2015.select(stats_2015.col(stat)).first().getString(0).toDouble
-    val stat_2016 = stats_2016.select(stats_2016.col(stat)).first().getString(0).toDouble
-    val stat_2017 = stats_2017.select(stats_2017.col(stat)).first().getString(0).toDouble
+    val stat_2015 = stats_2015.select(stats_2015.col(stat)).first().getDouble(0)
+    val stat_2016 = stats_2016.select(stats_2016.col(stat)).first().getDouble(0)
+    val stat_2017 = stats_2017.select(stats_2017.col(stat)).first().getDouble(0)
 
 
     val all_stats =  List(stat_2015, stat_2016, stat_2017)
     all_stats
     }
-
 
   def maxDf(parameter: String): Double = {
     val list_max = getStatistic(parameter, "max")
@@ -58,4 +51,44 @@ class ProcessData(df: DataFrame, s: SparkSession){
     avg
   }
 
+  def maxDfYear(parameter: String, year: Int): Double = {
+    val list_max = getStatistic(parameter, "max")
+    val max = list_max(year)
+    max
   }
+  def minDfYear(parameter: String, year: Int): Double = {
+    val list_min = getStatistic(parameter, "min")
+    val min = list_min(year)
+    min
+  }
+  def avgDfYear(parameter: String, year: Int): Double = {
+    val list_avg = getStatistic(parameter, "avg")
+    val avg = list_avg(year)
+    avg
+  }
+
+
+  def getLine(parameter:String, f: (String, Int) => Double) = {
+    val year_2015 = f(parameter, 0).toString
+    val year_2016 = f(parameter, 1).toString
+    val year_2017 = f(parameter, 2).toString
+    (parameter, year_2015, year_2016, year_2017)
+  }
+
+  def displayDf(f: (String, Int) => Double, param: String)  = {
+    val gov = getLine("Gov", f)
+    val eco = getLine("Economy", f)
+    val family = getLine("Family", f)
+    val score = getLine("Score", f)
+
+    val my_list = Seq(gov, eco, family, score)
+    val minDf = spark.createDataFrame(my_list).toDF("Parameter",
+                param + "_2015",
+                param + "_2016",
+                param + "_2017")
+    minDf.show()
+  }
+
+
+
+}
--- src/main/scala/hapiness/main.scala
+++ src/main/scala/hapiness/main.scala
@@ -1,7 +1,6 @@
 package hapiness
 import org.apache.spark.sql.SparkSession
 
-
 object HappinessData extends App {
   val spark = SparkSession
     .builder()
@@ -17,12 +16,27 @@ object HappinessData extends App {
 
   val dataframeProcessor = new ProcessData(loadedDf, spark)
 
-  val gov = dataframeProcessor.maxDf("Gov")
-  print(gov)
-  // val family = dataframeProcessor.statistics("Family")
-  // val economy = dataframeProcessor.statistics("Economy")
-  // val score = dataframeProcessor.statistics("Score")
 
+  val max_gov = dataframeProcessor.maxDf("Gov")
+  val min_gov = dataframeProcessor.minDf("Gov")
+  val avg_gov = dataframeProcessor.avgDf("Gov")
+
+  val max_score = dataframeProcessor.maxDf("Score")
+  val min_score = dataframeProcessor.minDf("Score")
+  val avg_score = dataframeProcessor.avgDf("Score")
+
+  val max_eco = dataframeProcessor.maxDf("Economy")
+  val min_eco = dataframeProcessor.minDf("Economy")
+  val avg_eco = dataframeProcessor.avgDf("Economy")
+
+  val max_family = dataframeProcessor.maxDf("Family")
+  val min_family = dataframeProcessor.minDf("Family")
+  val avg_family = dataframeProcessor.avgDf("Family")
+
+
+  dataframeProcessor.displayDf(dataframeProcessor.maxDfYear, "max")
+  dataframeProcessor.displayDf(dataframeProcessor.minDfYear, "min")
+  dataframeProcessor.displayDf(dataframeProcessor.avgDfYear, "avg")
 
 
 }
